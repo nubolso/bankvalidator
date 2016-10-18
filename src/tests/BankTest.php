0d148571745aeca81392c82f94fcbafaef76ed5f
geninhocell <geninhocell@gmail.com>
2016-10-18 11:45:58 -0200

    Add BankTest.php

---
+++ src/tests/BankTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Genivaldo
+ * Date: 18/10/2016
+ * Time: 11:45
+ */
+
+namespace tests;
+
+
+class BankTest extends \PHPUnit_Framework_TestCase
+{
+
+}
